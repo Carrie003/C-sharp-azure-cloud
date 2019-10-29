9e4e06858025acabcde57b52d41f3a064e4a2301
Carrie003 <46972243+Carrie003@users.noreply.github.com>
2019-10-29 16:49:36 -0500

    Update AzureSubscription.cs

--- AzureSubscription.cs
+++ AzureSubscription.cs
@@ -4,4 +4,25 @@
     {
 
     }
+      public partial class CreateContainerForm : Form
+    {
+        public string ContainerName { get; set; }
+
+        public CreateContainerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            ContainerName = textBoxContainerName.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
 }
